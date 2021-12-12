4ee105dcd324ce0b41a3884d42d63e06d16f06eb
Philipp Arndt <2f.mail@gmx.de>
2021-12-12 11:08:13 +0100

    junit 4 stub for test containers

--- src/de.rnd7.mqttgateway.test/src/main/java/org/junit/rules/TestRule.java
+++ src/de.rnd7.mqttgateway.test/src/main/java/org/junit/rules/TestRule.java
@@ -1,7 +1,7 @@
 package org.junit.rules;
 
-import org.junit.runner.Description;
 import org.junit.runners.model.Statement;
+import org.junit.runner.Description;
 
 public interface TestRule {
     Statement apply(Statement var1, Description var2);
