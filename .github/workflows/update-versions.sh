42cf4960391a0132e34c34e64bc8a50663543281
Philipp Arndt <2f.mail@gmx.de>
2021-12-18 11:54:51 +0100

    add script to create PRs on dependent projects

---
+++ .github/workflows/update-versions.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+set -e
+cd "$(dirname "$0")"
+VERSION=$(gh release list | grep Latest | awk '{print $1}')
+echo "Update to version $VERSION"
+mkdir build
+pushd build
+  repos=( mqtt-to-telegram )
+  for REPO in "${repos[@]}"
+  do
+    gh repo clone "mqtt-home/$REPO"
+      pushd "$REPO/src"
+        git checkout -b "mqttgateway-$VERSION"
+        mvn versions:use-dep-version \
+          -DdepVersion="$VERSION" \
+          -Dincludes=de.rnd7.mqttgateway \
+          -DforceVersion=true \
+          -DprocessDependencies=true \
+          -DgenerateBackupPoms=false
+
+        git add .
+        git commit -m"Update to mqttgateway $VERSION"
+        git push --set-upstream origin mqttgateway-2.3.1
+        gh pr create --title "Dependency: Update to mqttgateway $VERSION" --body "Dependency: Update to mqttgateway $VERSION"
+      popd
+  done
+popd
